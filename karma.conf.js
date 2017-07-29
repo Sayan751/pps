af6766ccc60cefe49232cede11fbb2114bfe6a9a
Sayan Pal <sayan751@gmail.com>
2017-07-29 10:51:16 +0200

    Adding dev dep to phantomjs launcher for travis

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
         colors: true, // enable / disable colors in the output (reporters and logs)
         logLevel: config.LOG_INFO, // level of logging. possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         autoWatch: true, // enable / disable watching file and executing tests whenever any file changes
-        browsers: ['Chrome', 'Firefox', 'IE'], // start these browsers. available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS', 'Chrome', 'Firefox', 'IE'], // start these browsers. available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         singleRun: false, // Continuous Integration mode: if true, Karma captures browsers, runs the tests and exits
         concurrency: Infinity, // Concurrency level: how many browser should be started simultaneous
         karmaTypescriptConfig: {

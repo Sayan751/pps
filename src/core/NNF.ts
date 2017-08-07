f709daab5039930268d320549fe3d89273ea283b
Sayan Pal <sayan751@gmail.com>
2017-08-07 13:01:54 +0200

    Adding toString and variableSet for NNF

--- src/core/NNF.ts
+++ src/core/NNF.ts
@@ -2,22 +2,55 @@ import { CNF } from "./CNF";
 import { ClausalConnectives, Connectives } from "./Constants";
 import { Literal } from "./Literal";
 export class NNF {
-
+    /**
+     * Returns the set of variables used in the nnf.
+     *
+     * @type {Set<string>}
+     * @memberof Clause
+     */
+    public readonly variableSet: Set<string>;
     constructor(public group: Literal[] | NNF[], public connective?: ClausalConnectives) {
         if (group.length > 1 && !connective) { throw new Error("Connective is required for multiple items in the group"); }
+
+        if (this.isLiteralGroup(group)) {
+            this.variableSet = new Set<string>(group.map((lit) => lit.variable));
+        } else if (this.isNNFGroup(group)) {
+            this.variableSet = group
+                .reduce((acc: Set<string>, nnf: NNF) => {
+                    Array.from(nnf.variableSet)
+                        .forEach((variable: string) => acc.add(variable));
+                    return acc;
+                }, new Set<string>());
+        }
+    }
+    public toString(): string {
+        return `(` +
+            (this.isLiteralGroup(this.group)
+                ? this.group.map((item: Literal) => item.toString()).join(` ${this.connective} `)
+                : this.isNNFGroup(this.group)
+                    ? this.group.map((item: NNF) => item.toString()).join(` ${this.connective} `)
+                    : "")
+            + `)`;
     }
 
-/*
-    NNF schema:
-    1. Negation symbol in front of variable is only allowed.
-    2. A group of literal connected with connectives is in NNF.
-        2.1. A clause can be a group (clause can be both disjunctive and conjunctive)
-        2.2. A group is a collection of clause and literals.
-        2.3. Every group has homogeneous connective.
-             Every group needs to be formed as per boolean binding rule.
-    3. A literal is in NNF.
-    4. A NNF can either be a literal or a group (as describe above).
-*/
+    private isLiteralGroup(group: Literal[] | NNF[]): group is Literal[] {
+        return (group as Literal[])[0].variable !== undefined;
+    }
+    private isNNFGroup(group: Literal[] | NNF[]): group is NNF[] {
+        return (group as NNF[])[0].group !== undefined;
+    }
+
+    /*
+        NNF schema:
+        1. Negation symbol in front of variable is only allowed.
+        2. A group of literal connected with connectives is in NNF.
+            2.1. A clause can be a group (clause can be both disjunctive and conjunctive)
+            2.2. A group is a collection of clause and literals.
+            2.3. Every group has homogeneous connective.
+                 Every group needs to be formed as per boolean binding rule.
+        3. A literal is in NNF.
+        4. A NNF can either be a literal or a group (as describe above).
+    */
     /**
      * Parses the given string to equivalent NNF.
      * Throws error if the input string is invalid.
--- test/core/NNF.spec.ts
+++ test/core/NNF.spec.ts
@@ -10,18 +10,22 @@ describe("NNF test suite", () => {
         it("The literal x should be in NNF", () => {
             const nnf = new NNF([new Literal("x")]);
             expect(nnf).toBeDefined();
+            expect(nnf.variableSet.size).toBe(1);
         });
         it(`The literal ${Connectives.not}x should be in NNF`, () => {
             const nnf = new NNF([new Literal("x", true)]);
             expect(nnf).toBeDefined();
+            expect(nnf.variableSet.size).toBe(1);
         });
         it(`The literals x,y connected by OR should be in NNF`, () => {
             const nnf = new NNF([new Literal("x"), new Literal("y")], Connectives.or);
             expect(nnf).toBeDefined();
+            expect(nnf.variableSet.size).toBe(2);
         });
         it(`The literals x,y connected by AND should be in NNF`, () => {
             const nnf = new NNF([new Literal("x"), new Literal("y")], Connectives.and);
             expect(nnf).toBeDefined();
+            expect(nnf.variableSet.size).toBe(2);
         });
         it(`The groups ([x,y], AND), ([a,b], OR) connected by AND should be in NNF`, () => {
             const nnf = new NNF([
@@ -29,12 +33,39 @@ describe("NNF test suite", () => {
                 new NNF([new Literal("a"), new Literal("b")], Connectives.and)
             ], Connectives.and);
             expect(nnf).toBeDefined();
+            expect(nnf.variableSet.size).toBe(4);
         });
         it(`The groups ([x,y], AND), ([a,b], OR) without any connective should cause error`, () => {
             expect(() => new NNF([
                 new NNF([new Literal("x"), new Literal("y")], Connectives.and),
-                new NNF([new Literal("a"), new Literal("b")], Connectives.and)
+                new NNF([new Literal("a"), new Literal("b")], Connectives.or)
             ])).toThrowError();
+        });
+    });
+
+    describe("String representation of NNF object should be correct", () => {
+        it("The literal x should be (x) in string representation", () => {
+            const nnf = new NNF([new Literal("x")]);
+            expect(nnf.toString()).toBe(`(x)`);
+        });
+        it(`The literal ${Connectives.not}x should be (${Connectives.not}x) in string representation`, () => {
+            const nnf = new NNF([new Literal("x", true)]);
+            expect(nnf.toString()).toBe(`(${Connectives.not}x)`);
+        });
+        it(`The literals x,y connected by OR should be (x ${Connectives.or} y) in string representation`, () => {
+            const nnf = new NNF([new Literal("x"), new Literal("y")], Connectives.or);
+            expect(nnf.toString()).toBe(`(x ${Connectives.or} y)`);
+        });
+        it(`The literals x,y connected by AND should be (x ${Connectives.and} y) in string representation`, () => {
+            const nnf = new NNF([new Literal("x"), new Literal("y")], Connectives.and);
+            expect(nnf.toString()).toBe(`(x ${Connectives.and} y)`);
+        });
+        it(`The groups ([x,y], AND), ([a,b], OR) connected by AND should be should be ((x ${Connectives.and} y) ${Connectives.and} (a ${Connectives.or} b)) in string representation`, () => {
+            const nnf = new NNF([
+                new NNF([new Literal("x"), new Literal("y")], Connectives.and),
+                new NNF([new Literal("a"), new Literal("b")], Connectives.or)
+            ], Connectives.and);
+            expect(nnf.toString()).toBe(`((x ${Connectives.and} y) ${Connectives.and} (a ${Connectives.or} b))`);
         });
     });
 });
